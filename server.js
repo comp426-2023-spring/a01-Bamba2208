02fc9e9768b5d501927c5b4c7b7ef225a1a01a23
Ahmadou Diouf <89354887+Bamba2208@users.noreply.github.com>
2023-04-25 18:55:25 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require("fs");
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000;
+fs.readFile('./public/index.html','utf8', (err, data) => {
+    if (err) {
+      console.error();
+      return;
+    }
+    console.log(data);
+
+
+    const server = http.createServer((req, res) => {
+      // res.statusCode=200;
+      // res.setHeader('Content-Type', 'text/html');
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  
+  server.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+  });
+  });
